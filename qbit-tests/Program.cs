7dc929e59023ce006b10579f88110db2ce9339f6
Jon <jonathan.jackson@sgsco.com>
2020-09-09 00:09:56 +0100

    Add project files.

---
+++ qbit-tests/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace qbit_tests {
+
+    internal class Program {
+
+        public static async Task Main(string[] args) {
+            /////
+            // Settings...
+            /////
+            var address = "http://192.168.1.7:8080";
+            var userName = "admin";
+            var password = "adminadmin";
+            /////
+
+            var handler = new HttpClientHandler();
+            handler.UseCookies = false;
+            var client = new HttpClient(handler);
+
+            string body = string.Empty;
+            HttpResponseMessage response = null;
+
+            // 1. Should Fail Auth.
+            using (HttpRequestMessage authToFail = new HttpRequestMessage(HttpMethod.Post, $"{address}/api/v2/auth/login")) {
+                body = $"username=xxx&password=xx";
+                authToFail.Content = new StringContent(body, Encoding.UTF8, "application/x-www-form-urlencoded");
+                authToFail.Content.Headers.Add("Content-Length", body.Length.ToString());
+                authToFail.Headers.Add("Referer", address);
+
+                response = await client.SendAsync(authToFail);
+                if (response.Headers.Any(x => string.Equals(x.Key, "Set-Cookie", StringComparison.OrdinalIgnoreCase)))
+                    WriteLineColored("WRONG AUTH PASSED!", ConsoleColor.Red);
+                else
+                    WriteLineColored("CORRECT AUTH PASSED!", ConsoleColor.Green);
+            }
+
+            // 2. Should Pass Auth.
+            using (var authToPass = new HttpRequestMessage(HttpMethod.Post, $"{address}/api/v2/auth/login")) {
+                body = $"username={userName}&password={password}";
+                authToPass.Content = new StringContent(body, Encoding.UTF8, "application/x-www-form-urlencoded");
+                authToPass.Content.Headers.Add("Content-Length", body.Length.ToString());
+                authToPass.Headers.Add("Referer", address);
+
+                response = await client.SendAsync(authToPass);
+                if (response.IsSuccessStatusCode)
+                    WriteLineColored("CORRECT AUTH PASSED!", ConsoleColor.Green);
+                else
+                    WriteLineColored("WRONG AUTH PASSED!", ConsoleColor.Red);
+            }
+
+            string sid = response.Headers.FirstOrDefault(x => string.Equals(x.Key, "Set-Cookie", StringComparison.OrdinalIgnoreCase)).Value.FirstOrDefault();
+
+            if (sid == null) {
+                WriteLineColored("Cannot continue due to failed authentication.", ConsoleColor.Red);
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            // 3. Check we can get torrents.
+            Torrent[] torrents = Array.Empty<Torrent>();
+            using (var request = new HttpRequestMessage(new HttpMethod("GET"), $"{address}/api/v2/torrents/info")) {
+                request.Headers.TryAddWithoutValidation("Cookie", $"{sid}");
+
+                response = await client.SendAsync(request);
+                if (response.IsSuccessStatusCode) {
+                    torrents = JsonSerializer.Deserialize<Torrent[]>(await response.Content.ReadAsStringAsync());
+                    WriteLineColored($"Success getting torrents: {string.Join(", ", torrents.Select(x => x.name))}", ConsoleColor.Green);
+                }
+                else
+                    WriteLineColored("Failed to get torrents.", ConsoleColor.Red);
+            }
+
+            if (!torrents.Any()) {
+                WriteLineColored("Cannot continue as no torrents were found.", ConsoleColor.Red);
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            // 4. Pause the torrent.
+            string hash = torrents.First().hash;
+            using (var request = new HttpRequestMessage(new HttpMethod("GET"), $"{address}/api/v2/torrents/pause?hashes={hash}")) {
+                request.Headers.TryAddWithoutValidation("Cookie", $"{sid}");
+                request.Content = new StringContent(body, Encoding.UTF8, "application/x-www-form-urlencoded");
+                request.Content.Headers.Add("Content-Length", body.Length.ToString());
+
+                response = await client.SendAsync(request);
+                if (response.IsSuccessStatusCode) {
+                    WriteLineColored($"Success pausing torrent!", ConsoleColor.Green);
+                }
+                else
+                    WriteLineColored("Failed to pause torrent.", ConsoleColor.Red);
+            }
+
+            WriteLineColored("Complete!", ConsoleColor.Blue);
+            Console.Read();
+        }
+
+        private static void WriteLineColored(string value, ConsoleColor color) {
+            Console.ForegroundColor = color;
+            Console.WriteLine(value);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ qbit-tests/Torrent.cs
@@ -0,0 +1,48 @@
+﻿namespace qbit_tests {
+
+    public class Torrent {
+        public ulong added_on { get; set; }
+        public ulong amount_left { get; set; }
+        public bool auto_tmm { get; set; }
+        public float availability { get; set; }
+        public string category { get; set; }
+        public ulong completed { get; set; }
+        public ulong completion_on { get; set; }
+        public ulong dl_limit { get; set; }
+        public ulong dlspeed { get; set; }
+        public ulong downloaded { get; set; }
+        public ulong downloaded_session { get; set; }
+        public ulong eta { get; set; }
+        public bool f_l_piece_prio { get; set; }
+        public bool force_start { get; set; }
+        public string hash { get; set; }
+        public ulong last_activity { get; set; }
+        public string magnet_uri { get; set; }
+        public long max_ratio { get; set; }
+        public long max_seeding_time { get; set; }
+        public string name { get; set; }
+        public ulong num_complete { get; set; }
+        public ulong num_incomplete { get; set; }
+        public ulong num_leechs { get; set; }
+        public ulong num_seeds { get; set; }
+        public ulong priority { get; set; }
+        public float progress { get; set; }
+        public decimal ratio { get; set; }
+        public decimal ratio_limit { get; set; }
+        public string save_path { get; set; }
+        public long seeding_time_limit { get; set; }
+        public ulong seen_complete { get; set; }
+        public bool seq_dl { get; set; }
+        public ulong size { get; set; }
+        public string state { get; set; }
+        public bool super_seeding { get; set; }
+        public string tags { get; set; }
+        public ulong time_active { get; set; }
+        public ulong total_size { get; set; }
+        public string tracker { get; set; }
+        public long up_limit { get; set; }
+        public ulong uploaded { get; set; }
+        public ulong uploaded_session { get; set; }
+        public ulong upspeed { get; set; }
+    }
+}
